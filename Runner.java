4e0afa9e6209c467e8c1c3b1ada751d012b66696
Lucas Carpenter <lucas1021334@students.sbcsc.k12.in.us>
2022-02-08 10:30:09 -0500

    adding fake review

--- Runner.java
+++ Runner.java
@@ -3,7 +3,10 @@ import org.w3c.dom.Text;
 // import Review; 
 public class Runner {
   public static void main(String[] args){
-    System.out.println(Review.sentimentVal(("terrible")));
+    System.out.println(Review.starRating("jamesLove.txt"));
 
+    double num = Review.sentimentVal("warm");
+
+    // System.out.println(Review.totalSentiment("SimpleReview.txt"));
   }
 }
